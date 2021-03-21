26eccb9bb7010d384f23c2e8a93e3b4f74ebe4bb
Roshni Kumari Gupta <roshnigupta1996@gmail.com>
2021-03-21 13:20:53 +0530

    changed the picture and naming conventions

--- server/config/index.js
+++ server/config/index.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 module.exports = {
   development: {
-    sitename: 'The Colour Of The Stolen Heart [Development]',
+    sitename: 'The Colour Of a Stolen Heart [Development]',
     data: {
       speakers: path.join(__dirname, '../data/speakers.json'),
       feedback: path.join(__dirname, '../data/feedback.json'),
     },
   },
   production: {
-    sitename: 'The Colour Of The Stolen Heart',
+    sitename: 'The Colour Of a Stolen Heart',
     data: {
       speakers: path.join(__dirname, '../data/speakers.json'),
       feedback: path.join(__dirname, '../data/feedback.json'),
